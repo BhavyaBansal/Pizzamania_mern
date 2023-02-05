da29b5d429e0c4d237febb91000fd278e7118877
Bhavya Bansal <bhavyabansal0916@gmail.com>
2023-02-05 23:54:24 +0530

    Add Card Page Login Register

--- server.js
+++ server.js
@@ -5,14 +5,23 @@ const expressLayout = require('express-ejs-layouts')
 const path = require('path');
 // if port is not available we will write this
 const PORT = process.env.PORT || 3000
-app.get('/',(req,res)=>{
-    res.render('home');
-})
 //Assets
 app.use(express.static('public'))
 app.use(expressLayout)
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
+app.get('/',(req,res)=>{
+    res.render('home');
+})
+app.get('/cart',(req,res)=>{
+    res.render('customers/cart');
+})
+app.get('/login',(req,res)=>{
+    res.render('auth/login');
+})
+app.get('/register',(req,res)=>{
+    res.render('auth/register');
+})
 app.listen(PORT,()=>{
     console.log(`Listening on port ${PORT}`);
 })
